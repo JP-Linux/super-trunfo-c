238f1e7dc47178636562556eab4f647f786e9438
Anastasiia Urvantseva <ana.urvantseva@gmail.com>
2025-09-10 19:27:28 +0000

    adicionar Super Poder e Comparar as Cartas

--- super_trunfo.c
+++ super_trunfo.c
@@ -6,12 +6,13 @@ int main() {
     int numeroCodigo1, numeroCodigo2;
     char codigo1[10], codigo2[10];
     char nome1[30], nome2[30];
-    int populacao1, populacao2;
+    unsigned long int populacao1, populacao2;
     float area1, area2;
     float PIB1, PIB2;
     int pontosTuristicos1, pontosTuristicos2;
     float densidade1, densidade2;
     float PIBperCapita1, PIBperCapita2;
+    float superPoder1, superPoder2;
 
 
     //vamos inserir os dados da Carta 1
@@ -36,9 +37,10 @@ int main() {
     printf("Digite Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos1);
 
-    //vamos calcular Densidade populacional e PIB per capita em reias
+    //vamos calcular Densidade populacional e PIB per capita em reias Super Poder:
     densidade1 = (float) populacao1 / area1;
     PIBperCapita1 = (float) (PIB1 *1000000000)/ populacao1;
+    superPoder1 = (float) (populacao1 + area1 + PIB1 + pontosTuristicos1 + PIBperCapita1 + 1/densidade1);
 
     //vamos inserir os dados da Carta 2
     printf("\nCarta 2:\nDigite Estado (ex: A): ");
@@ -62,30 +64,41 @@ int main() {
     printf("Digite Número de Pontos Turísticos: ");
     scanf("%d", &pontosTuristicos2);
 
-    //vamos calcular Densidade populacional e PIB per capita em reias
+    //vamos calcular Densidade populacional e PIB per capita em reias e Super Poder
     densidade2 = (float) populacao2 / area2;
     PIBperCapita2 = (float) (PIB2 *1000000000)/ populacao2;
+    superPoder2 = (float) (populacao2 + area2 + PIB2 + pontosTuristicos2 + PIBperCapita2 + 1/densidade2);
 
+    //Exibir os Resultados com Comparações
+    printf("População: %s (%lu)\n", (populacao1 > populacao2) ? "Carta 1 venceu" : "Carta 2 venceu", (populacao1 > populacao2) ? 1 : 0);
+    printf("Área: %s (%.0f)\n", (area1 > area2) ? "Carta 1 venceu" : "Carta 2 venceu", (area1 > area2) ? 1 : 0);
+    printf("PIB: %s (%.0f)\n", (PIB1 > PIB2) ? "Carta 1 venceu" : "Carta 2 venceu", (PIB1 > PIB2) ? 1 : 0);
+    printf("Pontos Turísticos: %s (%d)\n", (pontosTuristicos1 > pontosTuristicos2) ? "Carta 1 venceu" : "Carta 2 venceu", (pontosTuristicos1 > pontosTuristicos2) ? 1 : 0);
+    printf("Densidade Populacional: %s (%.0f)\n", (densidade1 < densidade2) ? "Carta 1 venceu" : "Carta 2 venceu", (densidade1 < densidade2) ? 1 : 0);
+    printf("PIB per Capita: %s (%.0f)\n", (PIBperCapita1 > PIBperCapita2) ? "Carta 1 venceu" : "Carta 2 venceu", (PIBperCapita1 > PIBperCapita2) ? 1 : 0);
+    printf("Super Poder: %s (%.0f)\n", (superPoder1 > superPoder2) ? "Carta 1 venceu" : "Carta 2 venceu", (superPoder1 > superPoder2) ? 1 : 0);
+    
 
     //vamos imprimir os dados da Carta 1
-    printf("\nCarta 1:\n");
+   /* printf("\nCarta 1:\n");
     sprintf(codigo1, "%c%02d", estado1, numeroCodigo1);
     printf("Estado: %c\nCódigo: %s\n", estado1, codigo1);
-    printf("Nome da Cidade: %s\nPopulação: %d\n", nome1, populacao1);
+    printf("Nome da Cidade: %s\nPopulação: %lu\n", nome1, populacao1);
     printf("Área: %.2f km²\nPIB: %.2f bilhões de reais\n", area1, PIB1);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
-    printf("PIB per Capita: %.2f reais\n\n", PIBperCapita1);
+    printf("PIB per Capita: %.2f reais\n\n", PIBperCapita1); */
+
 
     //vamos imprimir os dados da Carta 2
-    printf("Carta 2:\n");
+    /* printf("Carta 2:\n");
     sprintf(codigo2, "%c%02d", estado2, numeroCodigo2);
     printf("Estado: %c\nCódigo: %s\n", estado2, codigo2);
-    printf("Nome da Cidade: %s\nPopulação: %d\n", nome2, populacao2);
+    printf("Nome da Cidade: %s\nPopulação: %lu\n", nome2, populacao2);
     printf("Área: %.2f km²\nPIB: %.2f bilhões de reais\n", area2, PIB2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
     printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
-    printf("PIB per Capita: %.2f reais\n", PIBperCapita2);
+    printf("PIB per Capita: %.2f reais\n", PIBperCapita2);  */
 
     return 0;
 }
